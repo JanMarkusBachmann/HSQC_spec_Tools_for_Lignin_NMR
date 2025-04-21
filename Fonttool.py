8b637f589e1074c9ca3276200ccd75420a9c06a6
JanMarkusBachmann <146488952+JanMarkusBachmann@users.noreply.github.com>
2025-04-21 19:48:17 +0300

    toolkit grid working

--- Fonttool.py
+++ Fonttool.py
@@ -13,8 +13,8 @@ class App(ctk.CTk):
         self.currentfontdict = dict()
         self.currentfontdictsizes = dict()
         self.currentfontletter = ' '
-        self.fonteditor_pxfromzero_rows = 0
-        self.fonteditor_pxfromzero_cols = 0
+        self.fonteditor_pxfromzero_rows = 1
+        self.fonteditor_pxfromzero_cols = 1
         self.pxframe = Pixelframeing(self, self.fonteditor_pxfromzero_rows, self.fonteditor_pxfromzero_cols)
         self.pxframeletterentry = ctk.CTkEntry(self)
         self.pxframeletterentry.bind('<Key-Return>', self.pxframeletterchange)
@@ -48,24 +48,9 @@ class App(ctk.CTk):
         self.elemnts.append(ctk.CTkButton(self, text='Back', fg_color='red', hover_color='orange', command=self.firstwindows))
         self.repack()
     def createfont(self):
-        self.depack()
-        self.elemnts.append(ctk.CTkLabel(self, text="Please press on a pixle to activate(white) or deactivate(black) it"))
-        #self.elemnts.append(ctk.CTkEntry(self, textvariable=self.currentfontletter))
-        self.elemnts.append(ctk.CTkFrame(self))
-        rowincB = ctk.CTkButton(self.elemnts[-1], text='Increase row pixels', command=self.increaserow)
-        rowincB.pack(side='left', padx=10)
-        rowdecB = ctk.CTkButton(self.elemnts[-1], text='decrease row pixels', command=self.decreaserow)
-        rowdecB.pack(side='left', padx=10)
-        self.elemnts.append(ctk.CTkFrame(self))
-        colincB = ctk.CTkButton(self.elemnts[-1], text='Increase row pixels', command=self.increasecolumn)
-        colincB.pack(side='left', padx=10)
-        coldecB = ctk.CTkButton(self.elemnts[-1], text='decrease row pixels', command=self.decreasecolumn)
-        coldecB.pack(side='left', padx=10)
-        self.elemnts.append(self.pxframelettermessage)
-        self.elemnts.append(self.pxframeletterentry)
-        self.elemnts.append(self.pxframe)
-
-        self.repack()
+        self.currentfontdict.clear()
+        self.currentfontdictsizes.clear()
+        self.fonteditor()
     def increaserow(self):
         self.pxframe.nrows += 1
         self.pxframe.gridreset()
@@ -107,18 +92,24 @@ class App(ctk.CTk):
                 if isdict and line == """    ,' ': tuple()\n""":
                     self.currentfontdict.update({' ': tuple()})
                 elif isdict:
-                    let = line.strip('\t').strip('\n').split(':')[0].strip(' ').strip(',')
+                    let = line.strip('\t').strip('\n').split(':')[0].strip(' ').strip(',').strip("'")
                     RN = []
                     for cord in line.strip('\t').strip('\n').split(':')[1].split('), '):
                         temp = (cord.strip(' ').strip('(').strip('(').strip(')').strip(')').split(', '))
                         RN.append(tuple((int(temp[0]), int(temp[1].strip('),')))))
                     self.currentfontdict.update({let: tuple(RN)})
                 if issizedict:
-                    let = line.strip('\t').strip('\n').split(':')[0].strip(' ').strip(',')
+                    let = line.strip('\t').strip('\n').split(':')[0].strip(' ').strip(',').strip("'")
                     RN = []
                     for cord in line.strip('\t').strip('\n').split(':')[1].split('), '):
                         temp = (cord.strip(' ').strip('(').strip('(').strip(')').strip(')').split(', '))
                         RN.append(tuple((int(temp[0]), int(temp[1]))))
+                        #create minmax values for current font rows and cols
+                        if abs(int(temp[0])) > self.fonteditor_pxfromzero_rows:
+                            self.fonteditor_pxfromzero_rows = abs(int(temp[0]))
+                        if abs(int(temp[1])) > self.fonteditor_pxfromzero_cols:
+                            self.fonteditor_pxfromzero_cols = abs(int(temp[1]))
+
                     self.currentfontdictsizes.update({let: tuple(RN)})
                 if line[0:16] == 'letters = dict({':
                     isdict = True
@@ -129,7 +120,21 @@ class App(ctk.CTk):
         self.fonteditor()
     def fonteditor(self):
         self.depack()
+        self.pxframe.gridreset()
 
+        self.elemnts.append(ctk.CTkLabel(self, text="Please press on a pixle to activate(white) or deactivate(black) it"))
+        self.elemnts.append(ctk.CTkFrame(self))
+        rowincB = ctk.CTkButton(self.elemnts[-1], text='Increase row pixels', command=self.increaserow)
+        rowincB.pack(side='left', padx=10)
+        rowdecB = ctk.CTkButton(self.elemnts[-1], text='decrease row pixels', command=self.decreaserow)
+        rowdecB.pack(side='left', padx=10)
+        self.elemnts.append(ctk.CTkFrame(self))
+        colincB = ctk.CTkButton(self.elemnts[-1], text='Increase row pixels', command=self.increasecolumn)
+        colincB.pack(side='left', padx=10)
+        coldecB = ctk.CTkButton(self.elemnts[-1], text='decrease row pixels', command=self.decreasecolumn)
+        coldecB.pack(side='left', padx=10)
+        self.elemnts.append(self.pxframelettermessage)
+        self.elemnts.append(self.pxframeletterentry)
         self.elemnts.append(self.pxframe)
 
         self.repack()
@@ -147,16 +152,25 @@ class Pixelframeing(ctk.CTkFrame):
     def repack(self):
         for obj in self.rows:
             obj.pack(side='bottom')
-
     def depack(self):
         for obj in self.rows:
             obj.pack_forget()
         self.rows = []
-
     def gridreset(self):
         self.depack()
+        self.buttons.clear()
+        self.nrows = self.appmaster.fonteditor_pxfromzero_rows
+        self.ncols = self.appmaster.fonteditor_pxfromzero_cols
         for rownumb in range(-self.nrows, self.nrows + 1):
             self.rows.append(Pixelframeingrow(self, self.ncols, rownumb))
+
+            # check if currentfontdict and size are empty, if not, readin the first char
+        if len(self.appmaster.currentfontdict.keys()) != 0:
+            try:
+                for cord in self.appmaster.currentfontdict[self.appmaster.currentfontletter]:
+                    self.buttons[cord].statechange()
+            except KeyError:
+                self.appmaster.pxframelettermessage.configure(text='This letter has not been made')
 
         self.repack()
 
@@ -173,18 +187,15 @@ class Pixelframeingrow(ctk.CTkFrame):
     def repack(self):
         for obj in self.buttons:
             obj.pack(side='left')
-
     def depack(self):
         for obj in self.buttons:
             obj.pack_forget()
         self.buttons = []
-
     def gridreset(self):
         self.depack()
         for colnumb in range(-self.ncols, self.ncols + 1):
             self.buttons.append(Pxbutton(self, colnumb, self.rownumb))
         self.repack()
-
 
 class Pxbutton(ctk.CTkButton):
     def __init__(self, master, col, row, **kwargs):
@@ -205,8 +216,6 @@ class Pxbutton(ctk.CTkButton):
             self.configure(fg_color = 'black')
         else:
             self.configure(fg_color = 'white')
-
-
     def __del__(self):
         # Remove button from rowmaster.framemaster.buttons list if it exists
         if (self.row, self.col) in self.rowmaster.framemaster.buttons.keys():
