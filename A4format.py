b3e001d55763ed2655746961bfa56dd966e6425d
Jan Markus Bachmann <bachmannjanmarkus@gmail.com>
2025-05-13 00:22:50 +0300

    Added A4 format

--- A4format.py
+++ A4format.py
@@ -1,9 +1,9 @@
 import RenderingFRAME as fr
 import time
 
-spct = "HSQCdata/HSQC-frHL-EtOH-FEN-120325.txt"
+spct = "HSQCdata/HSQC-250404-EtOH-frHL-SA-ECH.txt"
 start = time.time()
-test = fr.FilePNG(2200, 2300, 16, background='w', name=spct.split('/')[-1].strip('.txt'))
+test = fr.FilePNG(2100, 2970, 16, background='w', name=spct.split('/')[-1].strip('.txt'))
 #test = fr.FilePNG(160, 154, 16, background='w', name='punane')
 #test.legacyletterwrite(40, 40, "10", 'd', scale=3)
 #test.legacyletterwrite(40-17, 40+40, "3", 'd', scale=2)
@@ -19,9 +19,9 @@ test.gradientgraph(spct,
                    (32000, 32000, 32000),
                    'd',
                    4,
-                   (150,200),
-                   (2000, 2000),
-                   25000,
+                   (150,150),
+                   (2500, 1950),
+                   20000,
                    True,
                    (65535, 0, 0),
                    (32000, 65535, 32000),
@@ -29,12 +29,12 @@ test.gradientgraph(spct,
                    (0, 0, 65535),
                    700,
                    4,
-                   (2230, 1000),
-                   (2280, 2000))
+                   (2830, 1000),
+                   (2880, 2000))
 #test.gradientgraph(spct, (2, 5),(30, 90),0.25,5,(32000, 32000, 32000),'d',4,(150,200),(2000, 2000),35000,True,(65535, 0, 0),(32000, 65535, 32000),(65535, 65535, 65535),(0, 0, 65535), 500, 5, (2200, 1000), (2250, 2000))
 test.legacyletterwrite(50, 1000, spct.split('/')[-1].strip('.txt'), 'd', 6)
-test.legacyletterwrite(1770, 90, '13C (ppm)', 'd', 4)
-test.legacyletterwrite(2100, 400, '1H (ppm)', 'd', 4)
+test.legacyletterwrite(2670, 90, '13C (ppm)', 'd', 4)
+test.legacyletterwrite(2600, 400, '1H (ppm)', 'd', 4)
 test.filewrite()
 end = time.time()
 elapsed = end - start
